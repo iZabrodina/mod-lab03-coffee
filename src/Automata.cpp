f0ea4d9d1d01dc5fb7748f507ccd2ec3d41ac64b
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-03-27 10:15:55 +0300

    Add files via upload

---
+++ src/Automata.cpp
@@ -0,0 +1,76 @@
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
+#include <iostream>
+
+Automata::Automata() {
+    cash = 0;
+    state = OFF;
+}
+
+void Automata::on() {
+    if (state == OFF) {
+        state = WAIT;
+    }
+}
+
+void Automata::off() {
+    if (state == WAIT) {
+        state = OFF;
+    }
+}
+
+void Automata::coin(int c) {
+    if (state == WAIT) {
+        state = ACCEPT;
+    }
+    cash = cash + c;
+}
+
+string* Automata::etMenu() {
+    return menu;
+}
+
+STATES Automata::getState() {
+    return state;
+}
+
+void Automata::choice(int c) {
+    if (state == ACCEPT) {
+        state = CHECK;
+        this->choice_user = c;
+        check();
+    }
+}
+
+void Automata::check() {
+    if (state == CHECK) {
+        if (cash - prices[choice_user - 1] >= 0)
+            cook();
+        else
+            state = ACCEPT;
+    }
+}
+
+int Automata::cancel() {
+    if (state == ACCEPT || state == CHECK || state == WAIT) {
+        state = WAIT;
+        int c = cash;
+        cash = 0;
+        return c;
+    }
+    else {
+        return 0;
+    }
+}
+
+void Automata::cook() {
+    if (state == CHECK) {
+        state = COOK;
+    }
+}
+
+void Automata::finish() {
+    state = WAIT;
+    cash = cash - prices[choice_user - 1];
+    choice_user = -1;
+}
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright 2022 UNN-IASR
+#include <iostream>
+#include "Automata.h"
+
+using std::string;
+using std::cout;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Automata a;
+    int b;
+    a.on();
+    cout << "Автомат готов принять заказ\n";
+    getchar();
+    a.coin(200);
+    cout << "Деньги получены, пожалуйста сделайте выбор напитка\n";
+    getchar();
+    a.choice(1);
+    cout << "Готовка...\n";
+    getchar();
+    a.finish();
+    b = a.cancel();
+    cout << "Ваша сдача " << b;
+    getchar();
+    a.off();
+    cout << "\nВыключение автомата...\n";
+}
