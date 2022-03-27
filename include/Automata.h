c2743521d4e2ebbdfc831ae079f5fade6bad3815
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-03-27 13:37:53 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -6,30 +6,29 @@ using std::string;
 enum STATES {
         OFF,
         WAIT,
-	COOK,
-	ACCEPT,
-	CHECK
+        COOK,
+        ACCEPT,
+        CHECK
 };
 
 class Automata {
-private:
-	int cash;
-	string menu[4] = { "Coffee", "White tea",
-	"Green tea", "Black tea" };
-	int prices[4] = { 149, 119, 99, 129 };
-	STATES state;
-	int choice_user;
+ private:
+        int cash;
+        string menu[4] = { "Coffee", "White tea", "Green tea", "Black tea" };
+        int prices[4] = { 149, 119, 99, 129 };
+        STATES state;
+        int choice_user;
 
-public:
-Automata();
-	void on();
-	void off();
-	void coin(int c);
-	string* etMenu();
-	STATES getState();
-	void choice(int c);
-	void check();
-	int cancel();
-	void cook();
-	void finish();
+ public:
+        Automata();
+        void on();
+        void off();
+        void coin(int c);
+        string* etMenu();
+        STATES getState();
+        void choice(int c);
+        void check();
+        int cancel();
+        void cook();
+        void finish();
 };
