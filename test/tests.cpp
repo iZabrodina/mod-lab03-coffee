75656f05cc4ec7eeace7aa34e9a76c36455b7715
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-03-27 13:39:31 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,55 +3,55 @@
 #include "Automata.h"
 
 TEST(taskA, test1) {
-	Automata a = Automata();
-	a.on();
-	int result = a.getState();
-	EXPECT_EQ(WAIT, result);
+        Automata a = Automata();
+        a.on();
+        int result = a.getState();
+        EXPECT_EQ(WAIT, result);
 }
 
 TEST(taskA, test2) {
-    Automata a = Automata();
-	a.on();
-	a.coin(100);
-	int result = a.getState();
-	EXPECT_EQ(ACCEPT, result);
+        Automata a = Automata();
+        a.on();
+        a.coin(100);
+        int result = a.getState();
+        EXPECT_EQ(ACCEPT, result);
 }
 
 TEST(taskA, test3) {
-	Automata a = Automata();
-	a.on();
-	a.coin(100);
-	a.choice(1);
-	int result = a.getState();
-	EXPECT_EQ(ACCEPT, result);
+        Automata a = Automata();
+        a.on();
+        a.coin(100);
+        a.choice(1);
+        int result = a.getState();
+        EXPECT_EQ(ACCEPT, result);
 }
 
 TEST(taskA, test4) {
-	Automata a = Automata();
-	a.on();
-	a.coin(200);
-	a.choice(1);
-	int result = a.getState();
-	EXPECT_EQ(COOK, result);
+        Automata a = Automata();
+        a.on();
+        a.coin(200);
+        a.choice(1);
+        int result = a.getState();
+        EXPECT_EQ(COOK, result);
 }
 
 TEST(taskA, test5) {
-    Automata a = Automata();
-	a.on();
-	a.coin(200);
-	a.choice(1);
-	a.finish();
-	int result = a.cancel();
-	EXPECT_EQ(51, result);
+        Automata a = Automata();
+        a.on();
+        a.coin(200);
+        a.choice(1);
+        a.finish();
+        int result = a.cancel();
+        EXPECT_EQ(51, result);
 }
 
 TEST(taskA, test6) {
-	Automata a = Automata();
-	a.on();
-	a.coin(200);
-	a.choice(1);
-	a.finish();
-	a.off();
-	int result = a.getState();
-	EXPECT_EQ(OFF, result);
+        Automata a = Automata();
+        a.on();
+        a.coin(200);
+        a.choice(1);
+        a.finish();
+        a.off();
+        int result = a.getState();
+        EXPECT_EQ(OFF, result);
 }
